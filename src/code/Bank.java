018e3dd37acaf3d15df49491efb1eb799f41454f
Sam Ordonez <samordonez.ce@gmail.com>
2024-01-15 07:34:53 -0800

    Fixed printAllCustomerData() syntax, added documentation

--- src/code/Bank.java
+++ src/code/Bank.java
@@ -1,42 +1,75 @@
 /**
- * Description goes here...
+ * A representation of a bank in a simple banking system;
+ * representing its own bank name, list of unique account numbers,
+ * account member names, and account monetary balance
  *
  * @author vindy, marius, sam ordonez
  * @version 1.0
  */
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 public class Bank {
-    private final String                    bankName;
+    private final String                   bankName;
     private final Map<String, BankAccount> bankAccounts;
 
+    /**
+     * @param bankName Represents the name of the bank.
+     */
     public Bank(final String bankName)
     {
         this.bankName = bankName;
         this.bankAccounts = new HashMap<>();
     }
 
+    /**
+     * Adds a new 'BankAccount' instance to the bank's accounts,
+     * using the account number as the key.
+     * @param newBankAccount a new Bank Account instance that has its own
+     *                       unique account number, account member name,
+     *                       and account monetray balance
+     */
     public void addAccount(BankAccount newBankAccount)
     {
         this.bankAccounts.put(newBankAccount.getAccountNumber(), newBankAccount);
     }
 
+    /**
+     * the unique account number of a bank account
+     * @param accountNumber the unique account number of a bank account
+     * @return the unique account number of a bank account
+     */
     public BankAccount getAccount(String accountNumber)
     {
         return this.bankAccounts.get(accountNumber);
     }
 
+    /**
+     * Removes the unique account number of a bank account from the list group
+     * @param accountNumber the unique account number of a bank account
+     */
     public void removeAccount(String accountNumber)
     {
         this.bankAccounts.remove(accountNumber);
     }
 
+    /**
+     * Access the number/size of bank accounts in the list group
+     * @return the number/size of bank accounts in the list group
+     */
     public int getNumberOfAccounts()
     {
         return this.bankAccounts.size();
     }
 
+    /**
+     * Access the total amount of money, in CAD,
+     * of all the bank accounts in the bank system
+     * @return the total amount of money, in CAD,
+     *         of all the bank accounts in the bank system
+     */
     public double getTotalAccountsBalance()
     {
         double totalCdn = 0.0;
@@ -46,16 +79,40 @@ public class Bank {
         return totalCdn;
     }
 
+    /**
+     * Deposits money, in CAD, to an account number
+     * @param amountCdn money, in CAD, to be deposited
+     * @param accountNumber the unique account number
+     *                      where the money will be deposited
+     */
     public void depositTo(double amountCdn, String accountNumber)
     {
         this.bankAccounts.get(accountNumber).depositMoneyCad(amountCdn);
     }
 
-    public void printAllCustomerData() {
-        for (BankAccount account : this.bankAccounts.values())
+    /**
+     * Iterates through the bank's accounts,
+     * printing customer information such as
+     * the last name, account balance, and account number.
+     */
+    public void printAllCustomerData()
+    {
+        final Iterator<String> it;
+        it = this.bankAccounts.keySet().iterator();
+
+        while (it.hasNext())
         {
-            System.out.printf("Customer %s has $%.2f in account #%s\n", account.getMemberLastName(), account.getBalanceCdn(), account.getAccountNumber());
+            final String accountNumber;
+            accountNumber= it.next();
+
+            final BankAccount account;
+            account = this.bankAccounts.get(accountNumber);
+
+            System.out.printf("Customer %s has $%.2f in account #%s\n",
+                    account.getMemberLastName(), account.getBalanceCdn(), accountNumber);
         }
-        System.out.printf("Total bank balance in all accounts for %s is %.2f\n", this.bankName, this.getTotalAccountsBalance());
+
+        System.out.printf("Total bank balance in all accounts for %s is %.2f\n",
+                this.bankName, this.getTotalAccountsBalance());
     }
 }
