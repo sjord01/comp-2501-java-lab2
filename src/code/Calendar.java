e3d937d6d18a8a47fb6c855583015eea6f407c93
Sam Ordonez <samordonez.ce@gmail.com>
2024-01-16 09:10:57 -0800

    Update Documentation for Date and Calendar classes

--- src/code/Calendar.java
+++ src/code/Calendar.java
@@ -1,33 +1,61 @@
+/**
+ * A Calendar program that contains and prints out
+ * months, days, and years from 1910 to 2025;
+ * assumption is that each month will have 30 days
+ *
+ * @author vindy, marius, sam ordonez
+ * @version 1.0
+ */
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 
 public class Calendar {
-    private ArrayList<Integer> years;
-    private String[] months;
-    private ArrayList<Integer> days;
-    private HashMap<Integer, Date> dates;
+    private final ArrayList<Integer>      years;
+    private final String[]                months;
+    private final ArrayList<Integer>      days;
+    private final HashMap<Integer, Date>  dates;
 
-    public Calendar() {
+    /**
+     * The method creates a Date object for every date from January 1, 1910 to December 30, 2025;
+     * Assumption is that each month has 30 days
+     * Each Date object is stored in a HashMap 'dates' instance variable
+     * January 1 1910 has key 1, January 2 1910 has key 2..., all the way up to December 30 2025 which has key 41760
+     *
+     */
+    public Calendar()
+    {
         years = new ArrayList<>();
-        for (int i = 1910; i <= 2025; i++) {
+        for (int i = 1910; i <= 2025; i++)
+        {
             years.add(i);
         }
 
-        months = new String[]{"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+        months = new String[]{  "January", "February", "March",
+                                "April", "May", "June", "July",
+                                "August", "September", "October",
+                                "November", "December"};
 
         days = new ArrayList<>();
-        for (int i = 1; i <= 30; i++) {
+        for (int i = 1; i <= 30; i++)
+        {
             days.add(i);
         }
 
         dates = new HashMap<>();
         int key = 1;
-        for (int year : years) {
-            for (String month : months) {
-                Iterator<Integer> iterator = days.iterator();
-                while (iterator.hasNext()) {
-                    int day = iterator.next();
+
+        for (int year : years)
+        {
+            for (String month : months)
+            {
+                final Iterator<Integer> it;
+                it = days.iterator();
+
+                while (it.hasNext())
+                {
+                    int day = it.next();
                     Date date = new Date(year, month, day);
                     dates.put(key, date);
                     key++;
@@ -36,6 +64,10 @@ public class Calendar {
         }
     }
 
+    /**
+     * Prints out dates from January 1, 1910 to December 30, 2025;
+     * Assumption is that each month has 30 days
+     */
     public void printCalendar() {
         for (int key : dates.keySet()) {
             Date date = dates.get(key);
--- src/code/Date.java
+++ src/code/Date.java
@@ -1,27 +1,66 @@
+/**
+ * A Date program that tells the month, day, and year of a specific date
+ *
+ * @author vindy, marius, sam ordonez
+ * @version 1.0
+ */
 public class Date {
-    private final int year;
-    private final String month;
-    private final int day;
+    private final int       year;
+    private final String    month;
+    private final int       day;
 
-    public Date(int year, String month, int day) {
+    /**
+     * @param year      The year of a specific date in YYYY format
+     * @param month     The month of a specific date in String format (e.g. "January", "February",...)
+     * @param day       The day of a specific date in D or DD format
+     */
+    public Date(final int       year,
+                final String    month,
+                final int       day)
+    {
         this.year = year;
         this.month = month;
         this.day = day;
     }
 
-    public int getYear() {
+    //Accessors
+
+    /**
+     * Access the specific month-day-year combination of a specific date
+     * @return  the specific month-day-year combination of a specific date
+     *          where the format is month(String), day(int), and year (int)
+     *          e.g. January 1, 1910
+     */
+    public String getDate()
+    {
+        return month + " " + day + ", " + year;
+    }
+
+
+    /**
+     * Access the year of a specific date in YYYY format
+     * @return the year of a specific date in YYYY format
+     */
+    public int getYear()
+    {
         return year;
     }
 
-    public String getMonth() {
+    /**
+     * Access the month of a specific date in String format
+     * @return the month of a specific date in String format
+     */
+    public String getMonth()
+    {
         return month;
     }
 
-    public int getDay() {
+    /**
+     * Access the day of a specific date in D or DD (int) format
+     * @return the day of a specific date in D or DD (int) format
+     */
+    public int getDay()
+    {
         return day;
-    }
-
-    public String getDate() {
-        return month + " " + day + ", " + year;
     }
 }
